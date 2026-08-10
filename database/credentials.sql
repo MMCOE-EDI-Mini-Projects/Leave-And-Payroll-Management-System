c849ddb84c7e2cbdb8853cc4cabbf43a7f2c1c65
Ayush Sadavarte <ayushsadavarte2024.comp@mmcoe.edu.in>
2026-08-10 11:07:55 +0530

    Create Employee table in credentials.sql

--- database/credentials.sql
+++ database/credentials.sql
@@ -1,5 +1,16 @@
 create database login;
 
-create table signup(
-    username varchar(60),
+create table Employee(
+    employee_id int(10) not null primary key,
+    employee_code varchar(30) not null,
+    first_name varchar(60) not null,
+    last_name varchar(60) not null,
+    date_of_birth date not null,
+    gender varchar(20),
+    email varchar(100),
+    phone_no varchar(15),
+    date_of_joining date not null,
+    employee_rank varchar(40),
+    created_at datetime not null,
+    updated_at datetime not null,
 )
